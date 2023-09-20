6ef09f3d195bd39fba11d99b693f7a4503126509
bhatti.ayesh04 <bhatti.ayesh04@gmail.com>
2023-09-20 14:12:07 +0500

    removing controller

--- app/controllers/api/v1/posts_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::PostsController < ApplicationController
-
-    before_action :authenticate_user!, only: [:create, :update, :destroy]
-    before_action :set_post, only: [:show, :update, :destroy]
-  
-end
