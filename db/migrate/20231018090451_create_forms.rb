c6bd7317295ef9734fb99b9c3e7f60c7510c1f87
bhatti.ayesh04 <bhatti.ayesh04@gmail.com>
2023-10-31 17:21:26 +0500

    adding rspec for form

--- db/migrate/20231018090451_create_forms.rb
+++ db/migrate/20231018090451_create_forms.rb
@@ -4,7 +4,7 @@ class CreateForms < ActiveRecord::Migration[7.0]
       t.string :email
       t.string :username
       t.string :title
-      t.integer :user_id,
+      t.integer :user_id
       t.string :status
 
       t.timestamps
