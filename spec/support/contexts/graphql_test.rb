94f3e6ec9ed6f087d33824f5a0d60e01f381d040
syedanabimam <syedanabimam@gmail.com>
2023-11-08 08:44:48 +0500

    Fix spec

--- spec/support/contexts/graphql_test.rb
+++ spec/support/contexts/graphql_test.rb
@@ -3,6 +3,6 @@ RSpec.shared_context 'GraphQL test' do
   let(:context) { { current_user: user, current_ability: Ability.new(user) } }
   let(:variables) { {} }
   let(:response) do
-    AgencyWatcherSchema.execute(query, context: context, variables: variables).deep_symbolize_keys
+    OptigateApiSchema.execute(query, context: context, variables: variables).deep_symbolize_keys
   end
 end
