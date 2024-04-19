c036e6840fefc9a9f86241bcdc7778fe739f3531
bhatti.ayesh04 <bhatti.ayesh04@gmail.com>
2024-04-19 23:11:52 +0500

    heroku fix app-rec

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+require 'dotenv/rails-now' if Rails.env.development? || Rails.env.test?
 Bundler.require(*Rails.groups)
 Dotenv::Railtie.load
 
