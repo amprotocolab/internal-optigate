f6f5419acc2b3936f3996918137263ca3b933573
bhatti.ayesh04 <bhatti.ayesh04@gmail.com>
2024-04-23 17:07:17 +0500

    fixing mutation error

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,12 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
-    include MutationFields::Dashboard
-    include MutationFields::AdsAccount
-    include MutationFields::AgencyIntegration
-    include MutationFields::Goal
-    include MutationFields::Integration
-    include MutationFields::Plan
-    include MutationFields::Project
     include MutationFields::User
     include MutationFields::Contact
     include MutationFields::SystemNotificationMutationFields
